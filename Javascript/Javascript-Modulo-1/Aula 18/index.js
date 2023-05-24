96b6d97d6108219d66b1fbecc58a288379bf89b9
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-05-23 21:30:50 -0300

    aula 18

---
+++ Javascript/Javascript-Modulo-1/Aula 18/index.js
@@ -0,0 +1,14 @@
+// const idade = prompt("informe sua idade");
+
+// if (idade > 18) {
+//   alert("Vc é maior de idade");
+// } else if (idade > 12) {
+//   alert("vc é menor de idade");
+// } else if (idade > 4) {
+//   alert("Vc é criança");
+// } else {
+//   alert("..");
+// }
+
+const resultado = (6 === 6) ? "Verdadeiro" : "Falso"
+alert(resultado)
