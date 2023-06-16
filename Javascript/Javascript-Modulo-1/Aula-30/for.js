b78cc8fbbce3ec5c317649c7a450b29f0e810984
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-16 19:58:12 -0300

    Estruturas de Repetição: For

---
+++ Javascript/Javascript-Modulo-1/Aula-30/for.js
@@ -0,0 +1,10 @@
+let indice = 0;
+// inicialização; condição; finalização
+for (alert("Inicializando o for"); indice <= 10; indice++) {
+  alert("Indice = " + indice);
+}
+let nome = "Nome nome" //tamanho -> 0, 1, 2, 3, 4,....
+//i = indice
+for(let i=0; i <= nome.length; i++) {
+  console.log(nome[i])
+}
