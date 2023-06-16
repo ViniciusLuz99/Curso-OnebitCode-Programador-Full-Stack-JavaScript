09686509e84ea5b08cce4019133e6e1492591e75
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-15 21:13:57 -0300

    Menu interativo 🔚

--- Javascript/Javascript-Modulo-1/Exercicio-5/menuInterativo.js
+++ Javascript/Javascript-Modulo-1/Exercicio-5/menuInterativo.js
@@ -1,11 +1,33 @@
-const menu = prompt(
-  "Digite numero de uma opção. \n Opção: 1 \n Opção: 2 \n Opção: 3 \n Opção: 4 \n Sair: 5"
-);
+let menu = "";
 
 do {
-  alert("A opção Escolhida é " + menu);
+  // alert("A opção Escolhida é " + menu);
   menu = prompt(
-    "Digite numero de uma opção. \n Opção: 1 \n Opção: 2 \n Opção: 3 \n Opção: 4 \n Sair: 5"
+    "Digite numero de uma opção." +
+      "\n Opção: 1" +
+      "\n Opção: 2" +
+      "\n Opção: 3" +
+      "\n Opção: 4" +
+      "\n Sair: 5"
   );
-} while (menu >= 4);
 
+  switch (menu) {
+    case "1":
+      alert("Opção 1 selecionada");
+      break;
+    case "2":
+      alert("Opção 2 selecionada");
+      break;
+    case "3":
+      alert("Opção 3 selecionada");
+      break;
+    case "4":
+      alert("Opção 4 selecionada");
+      break;
+    case "5":
+      alert("Encerrando....");
+      break;
+    default:
+      alert("Opção invalida");
+  }
+} while (menu !== "5");
