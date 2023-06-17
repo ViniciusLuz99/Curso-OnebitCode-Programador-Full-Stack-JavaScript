73773e7a2ab7b07f1faf5ce57f737aa639e6b7e7
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-17 11:25:25 -0300

    Arrays - Aula 34

---
+++ Javascript/Javascript-Modulo-1/Aula 34/Arrays.js
@@ -0,0 +1,10 @@
+const listaDeCompras = ["açucar", false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[2] = 7
+
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[10])
