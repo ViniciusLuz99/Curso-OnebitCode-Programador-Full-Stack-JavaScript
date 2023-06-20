5cb6618f3949eaf689e7d75b17cd6561f6d3d047
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-20 20:52:21 -0300

    Mudança em algumas pastas e criação de algumas

---
+++ Javascript/Javascript-Modulo-1/Exercicio-7/Fila.js
@@ -0,0 +1,15 @@
+/*
+Simulador de fila de espera
+deve:
+1-Mostrar um menu interativo
+2 Menu Contendo:
+Lista de Todos os pacientes, mostrando sua posição ao lado
+Novo pasciente, Pede o nome e adiciona ele ao fim da fila
+Consultar paciente, Que retira o primeiro da lista e mostra na tela o paciente consultado
+3-Opção de Sair, caso contrario deve retornar ao Menu
+*/
+
+const filaVirtual = [
+  ""
+]
+
