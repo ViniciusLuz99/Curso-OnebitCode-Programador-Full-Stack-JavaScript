751a8647d9532c240aa0c412f3b73edc4a7f7a61
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-21 21:31:07 -0300

    Finalizado de acordo com a resolução do exercício e documentado.

--- Javascript/Javascript-Modulo-1/Exercicio-7/Fila.js
+++ Javascript/Javascript-Modulo-1/Exercicio-7/Fila.js
@@ -9,38 +9,43 @@ Consultar paciente, Que retira o primeiro da lista e mostra na tela o paciente c
 3-Opção de Sair, caso contrario deve retornar ao Menu
 */
 
-let menu = "";
-
+// Inicialização da fila virtual com alguns pacientes
 let filaVirtual = ["João", "Maria", "Pedro", "Ana", "Carlos", "Laura", "Lucas"];
-let pacienteConsultado = "Em espera...";
+let menu = "";// Variável para armazenar a opção escolhida no menu
+let pacienteConsultado = "Em espera...";// Variável para armazenar o paciente em consulta
 
 do {
   let pacientes = "";
-  for (let i = 0; i <= filaVirtual.length; i++) {
-    pacientes += i + 1 + "º - " + filaVirtual[i] + "\n";
+  for (let i = 0; i < filaVirtual.length; i++) {
+    pacientes += (i + 1) + "º - " + filaVirtual[i] + "\n";
   }
-
+  // Exibição do menu interativo e informações sobre os pacientes
   menu = prompt(
-    "Bem Vindo!" +
-      "\n Em consulta: " +
-      pacienteConsultado +
-      "\nPacientes na fila:\n" +
-      pacientes +
-      "\nEscolha uma opção" +
-      "\n1 - Novo paciente" +
-      "\n2 - Consultar paciente" +
-      "\n3 - Sair"
+    "Escolha uma opção" +
+    "\n1 - Novo paciente" +
+    "\n2 - Consultar paciente" +
+    "\n3 - Sair" +
+   "\nPacientes na fila:\n" + pacientes +
+     "\n Em consulta: " + pacienteConsultado
   );
+
   switch (menu) {
     case "1":
+   // Opção para adicionar um novo paciente à fila
       const novoPaciente = prompt("Insira o nome do paciente:");
       filaVirtual.push(novoPaciente);
       break;
     case "2":
-      pacienteConsultado = filaVirtual.shift();
-      alert("Consultando " + pacienteConsultado);
+    // Opção para consultar o próximo paciente da fila
+      pacienteConsultado = filaVirtual.shift()// Remove o primeiro paciente da fila e armazena em pacienteConsultado
+      if (filaVirtual.length > 0) {
+        alert("Consultando " + pacienteConsultado);
+      } else {
+        alert("Não há pacientes na fila")
+      }
       break;
     case "3":
+    // Opção para sair do programa
       alert("Finalizando...");
       break;
     default:
