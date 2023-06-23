7ab495f16810419a283b995b397c40430fce2bb3
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-06-23 20:18:15 -0300

    Chat gpt desafio e aula sobre objetos, já que a do curso estava bem vaga.....

---
+++ Javascript/Javascript-Modulo-1/Objetos-ChatGPT/Objetos.js
@@ -0,0 +1,11 @@
+//Resolução do desafio que o ChatGPT me fez
+let carro = {
+  marca: "Volkswagen",
+  modelo: "Gol",
+  ano: 99,
+  ligarMotor: function () {
+    console.log("O carro está ligando");
+  },
+};
+
+carro.ligarMotor();
