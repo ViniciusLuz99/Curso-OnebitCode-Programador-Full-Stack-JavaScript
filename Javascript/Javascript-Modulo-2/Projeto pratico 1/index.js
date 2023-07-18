930b87947c142376daae743356c7e87c7e30f2f4
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-07-18 20:37:24 -0300

    Projeto prático: Calc.js - Começo
    Estrutura

---
+++ Javascript/Javascript-Modulo-2/Projeto pratico 1/index.js
@@ -0,0 +1,5 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+
