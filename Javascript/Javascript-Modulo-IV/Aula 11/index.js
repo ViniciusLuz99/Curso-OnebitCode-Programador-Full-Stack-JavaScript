017b10d75f2de006f36482ec0ec022bc2b91bbaf
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-23 10:10:00 -0300

    Create index.js

---
+++ Javascript/Javascript-Modulo-IV/Aula 11/index.js
@@ -0,0 +1,12 @@
+const a = 0; // em operações logicas o 0 é falso
+const b = null; // o null e falso
+const c = "test"; // em operações logicas o string "test" é verdadeiro
+
+console.log(a || b || c);
+
+let x = 0;
+let y = x || 34; //
+
+y = x ?? 42; 
+
+
